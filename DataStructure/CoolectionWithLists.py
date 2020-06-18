09b7f318cbac5abd8ccda24b861683b1e11ba86f
Elena <elena.ignat@live.com>
2020-06-18 18:44:55 +0300

    Update CollectionsWithLists.py file

--- DataStructure/CoolectionWithLists.py
+++ DataStructure/CoolectionWithLists.py
@@ -1,99 +1,119 @@
-import sys
 import random
-from math import sqrt as sqrt
 
 
 def main():
-
     """------------------List slice/split/obtain last_item---------"""
-    # convert string into a list
+    # convert a string type into a list type
     players = 'player_one, player_two, player_three, player_four'
+    print(type(players), 'players list:\t', players)
     players_list = players.split()
-    last_list_item = players_list[2]
-    print(last_list_item)
-    second_item_from_the_EOL = players_list[-2]
-    print(second_item_from_the_EOL)
-    slice_elements = players_list[0:2]
-    print(slice_elements)
+    print(type(players_list), 'players_list :\t', players_list)
 
+    # list slice example
+    last_item = players_list[3]
+    print("\nList last element:\t", last_item)
+
+    # list slice example with negative index
+    second_item = players_list[-3]
+    print('second item from the EOL:\t', second_item)
+
+    # slice a lists in a interval index
+    slice_elements = players_list[0:2]
+    print('slice elements of a list:\t', slice_elements)
 
     """------------------List append/insert/pop/sort/sorted/reverse/remove/del---------"""
     list_numbers = [3, 6, 2, 13, 1, 99, 100]
-    print('initial list\n' + str(list_numbers))
+    print('\ninitial list:\t', list_numbers)
     # add element into the list on the last position
     list_numbers.append(101)
-    print("Add element at the end of the list\n" + str(list_numbers))
+    print("updated list:\t" + str(list_numbers))
+
+    # sort the list in decreasing order
     list_numbers.sort(reverse=True)
-    print('Reverse the list\n' + str(list_numbers))
-    # delete the last element on the list
+    print('\nReverse list:\t' + str(list_numbers))
+
+    # delete the last element of the list
     list_numbers.pop()
-    print("Remover last element in a list\n" + str(list_numbers))
-    # add element on the list at a specific position
+    print("Remove last element in a list:\t", list_numbers)
+
+    # add element to the list at a specific position
     list_numbers.insert(2, 50)
-    print('Insert element at a specific position\n' + str(list_numbers))
+    print('Insert element at a specific position:\t', list_numbers)
+
+    # makes a permanent modification of the list
+    list_numbers.sort()
+    print('\npermanent sorted list_numbers in asc order:\t', list_numbers)
 
     # for a temporary sorted list we can use sorted method
-    print("temporally sorted list in descending order\n:", str(sorted(list_numbers, reverse=True)))
+    print("temporally sorted list in desc order\t:", sorted(list_numbers, reverse=True))
 
-
-    # display 5 elements from the list
-    print('Display first 5 element of the list\n' + str(list_numbers[0:4]))
     # delete an item of a list by position index
     del list_numbers[0]
-    print('Removed "1" item using del\n' + str(list_numbers))
+    print('\nRemoved "1" item using del:\t' + str(list_numbers))
+
     # remove accepts the value(int or str type) of the item and not the index
     list_numbers.remove(99)
-    print('Delete item "99" using remove method\n' + str(list_numbers))
-
+    print('Delete item "99" using remove method:\t', list_numbers)
 
     """------------------extend/min/max/sum of elements---------"""
+    # create a new list
     list_numbers_2 = [45, 21, 87, 36, 27]
-    print('List number 2\n' + str(list_numbers_2))
+    print('\nList number 2:\t', list_numbers_2)
+
     # merge 2 lists
     list_numbers.extend(list_numbers_2)
-    print('Bind list one with list two in one single list\n' + str(list_numbers))
+    print('Merge list_numbers with list_numbers_ in one single list:\t', list_numbers)
+
+    # sort the list in descending order
     list_numbers.sort(reverse=True)
-    print('Apply reverse method\n' + str(list_numbers))
+    print('Apply reverse method:\t', list_numbers)
+
     # return the maximum value from a list
     max_value = max(list_numbers)
-    print('maximum value: ' + str(max_value))
+    print('maximum value:\t', max_value)
+
     # return the minimum value from a list
     min_value = min(list_numbers)
-    print('minimum value: ' + str(min_value))
-    # make a sum of the entire values of the list
-    sum_list = sum(list_numbers)
-    print('sum of the entire value of the list: ' + str(sum_list))
+    print('minimum value:\t', min_value)
 
+    # obtain the sum of elem of the list
+    sum_list = sum(list_numbers)
+    print('sum of the entire value of the list:\t', sum_list)
 
     """-------------------retrieve the position of a specific value from the list------------"""
     list_seasons = ["summer", "spring", "winter", "autumn"]
-    print('retrieve the position of spring element from the seasons list: ' + str(list_seasons.index('spring')))
+    print('\nlist_seasons:\t', list_seasons)
+    # Retrieve the position of spring element from the seasons list
+    print('spring elem position:\t', list_seasons.index('spring'))
+
     # return random value from a list using choice function
     random_value = random.choice(list_seasons)
-    print('Extracted random value from season list: ' + str(random_value))
-    print("Seasons list\n" + str(list_seasons))
+    print('list random value:\t', random_value)
+    print("\nCurrent seasons list:\t", list_seasons)
     sorted_seasons = sorted(list_seasons)
-    print('Sorted seasons\n' + str(sorted_seasons))
+    print('Sorted seasons list:\t', sorted_seasons)
 
     """-----------------------transform a list in a string using join-----------------------"""
     seasons_str = ','.join(list_seasons)
-    print('Convert list in to one string = ' + str(seasons_str))
+    print('\n', type(seasons_str), seasons_str)
 
     """-----------------------custom format the list into desired pattern---------------"""
+    # Transform each elem of the list to start with capital letter and replace e with 3
     custom_list = [x.capitalize().replace('e', '3') for x in list_seasons]
-    print('Custom format list:\n' + str(custom_list))
+    print('\nCustom format list:\t' + str(custom_list))
+
     # remove quotes from the final list and convert into a str
     custom_list_1 = ', '.join(map(str, custom_list))
-    print('Convert list items into a string:\n' + custom_list_1)
+    print('Convert list items into a string:\t' + custom_list_1)
 
-    # check if a value is present in a specific list
-    print('Check {} is present in list = '.format('spring') + str('spring' in list_seasons))
+    print('filter_even_numbers:\t', filter_even_numbers())
 
 
-    """----------------------Iteration protocols over the list-----------------"""
-    seasons = ['winter', 'spring', 'summer', 'autumn']
+def filter_even_numbers():
+    input_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    even_numbers = list(filter(lambda x: x if not x % 2 in input_list else None, input_list))
+    return even_numbers
 
 
 if __name__ == '__main__':
     main()
-
